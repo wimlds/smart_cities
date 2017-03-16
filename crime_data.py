02588f218641a196594e41042cdc3bfea32a1595
chl557 <chl557@nyu.edu>
2017-03-16 00:13:00 -0400

    multi-index

--- crime_data.py
+++ crime_data.py
@@ -19,7 +19,7 @@ def crime_frame(file):
     frame_list = []
     
     for file_link in links:
-
+        temp = file_link.split("/")[-1].strip().split("_by")[0]
         df = pd.read_excel(file_link, 
                        header=[2], 
                        index_col=[0,1], 
@@ -29,15 +29,33 @@ def crime_frame(file):
         check_pct = [i for i in range(0,124)]
         check_pct.append("DOC")
         df = df[df["PCT"].isin(check_pct)].fillna(0)
+        df["file_type"] = temp
         frame_list.append(df)
 
+
     whole_frame = pd.concat(frame_list)
+    whole_frame = whole_frame[whole_frame["PCT"] != "DOC"]
+    whole_frame.PCT = whole_frame.PCT.astype(int)
 
     return whole_frame
 
 def main(input, output):
     whole_frame = crime_frame(input)
-    whole_frame.to_csv(output, index=False)
+
+    new_idx = []
+    name = [s for s in whole_frame.CRIME.unique() if "TOTAL" not in s]
+    total = [s for s in whole_frame.CRIME.unique() if "TOTAL" in s]
+    name.extend(total)
+    for prct in whole_frame.PCT.unique():
+        for types in whole_frame.file_type.unique():
+            for ct in name:
+                new_idx.append((prct, types, ct))
+
+    whole_frame.set_index(['PCT', "file_type", 'CRIME'], inplace=True)
+
+
+    whole_frame.reindex(new_idx).dropna().to_excel(sys.argv[2]+".xls")
+    whole_frame.reindex(new_idx).dropna().to_csv(sys.argv[2]+".csv")
 
 
 if __name__ == '__main__':
